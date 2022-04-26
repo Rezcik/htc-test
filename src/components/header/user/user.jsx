9f9ebd71fa59f6ba2b7728e0e1a8202ea7749ed6
Ilya <spaik-98@mail.ru>
2022-04-26 15:56:32 +0400

    Closing modal on a key

--- src/components/header/user/user.jsx
+++ src/components/header/user/user.jsx
@@ -7,7 +7,10 @@ export function User() {
     const[modal, setModal] = useState({type: '', active: false});
 
     return (
-        <div className={styles.user}>
+        <div className={styles.user}
+             onKeyDown={(e) => {
+                 e.key === 'Escape' ? setModal({...modal, active: false}) : null
+             }}>
             <Button name='Регистрация' type='registration' setModal={setModal} modal={modal}/>
             <Button name='Войти' type='authorization' setModal={setModal}/>
             <Modal setModal={setModal} modal={modal}/>
