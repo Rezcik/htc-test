e3636f816b025876b89e5064f63b93cc4912d16c
Ilya <spaik-98@mail.ru>
2022-04-28 19:10:26 +0400

    Transfer images from css to a component

--- src/pages/favorites/buttons/button.jsx
+++ src/pages/favorites/buttons/button.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import styles from './button.module.css';
 import {Link} from 'react-router-dom';
 
-export function Button({to, title}) {
+export function Button({to, title, img}) {
     return (
         <div className={styles.button}>
+            <img src={img} alt='back_img'/>
             <div className={styles.wrapper}>
                 <Link to={to}>
                     <span className={styles.title}>{title}</span>
--- src/pages/favorites/favourites.jsx
+++ src/pages/favorites/favourites.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import styles from './favourites.module.css';
-import {Title} from "../../components/title/title";
-import {Button} from "./buttons/button";
+import {Title} from '../../components/title/title';
+import {Button} from './buttons/button';
+import char_min from '../../assets/img/char_min.png';
+import loc_min from '../../assets/img/loc_min.png';
+import epi_min from '../../assets/img/epi_min.png';
 
 export function Favourites() {
     return (
@@ -9,11 +12,17 @@ export function Favourites() {
             <Title name='Избранное'/>
             <div className={styles.buttons}>
                 <Button to='locations'
-                        title='Локации'/>
+                        title='Локации'
+                        img={loc_min}
+                />
                 <Button to='characters'
-                        title='Персонажи'/>
+                        title='Персонажи'
+                        img={char_min}
+                />
                 <Button to='episodes'
-                        title='Эпизоды'/>
+                        title='Эпизоды'
+                        img={epi_min}
+                />
             </div>
             <div>
             </div>
